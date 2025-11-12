b2c003deb6e80fa1a45d093020f77aa43d0a85ed
jimtrung <nguyenhaitrung737@gmail.com>
2025-11-12 09:45:24 +0700

    feat: Final

--- Client.java
+++ Client.java
@@ -4,29 +4,20 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        /* NOTE: 
-            - Cổng của Server và Client phải giống nhau
-            - Thiết bị của Server và Client phải kết nối vào cùng 1 mạng
-        */
         int port = 1234;
 
-        /* Tạo "máy quét" để đọc dữ liệu đầu vào */
         Scanner sc = new Scanner(System.in); 
 
-        /* Yêu cầu client nhập địa chỉ IP của server */
         System.out.print("Nhập IP của server: ");
-        String host = sc.next();  
+        String serverIP = sc.next();  
 
-        // host = "192.168.1.103";
+        // host = "10.43.165.169"; 
 
-        /* Tạo kết nối với server */
-        Socket socket = new Socket(host, port);
+        Socket socket = new Socket(serverIP, port);
 
-        /* Tạo luồng đọc và ghi để trao đổi dữ liệu với server */
         BufferedReader in = new BufferedReader( new InputStreamReader(socket.getInputStream(), "UTF-8"));
         BufferedWriter out = new BufferedWriter( new OutputStreamWriter(socket.getOutputStream(), "UTF-8"));
 
-        /* Nhập và gửi dữ liệu */
         System.out.print("Nhập a: ");
         double a = sc.nextDouble();
 
@@ -39,11 +30,9 @@ public class Client {
         out.write(b + "\n");
         out.flush();
 
-        /* Nhận kết quả */
         String result = in.readLine(); 
         System.out.println("Server trả về: " + result);
 
-        /* Đóng kết nối socket và scanner */
         socket.close();
         sc.close();
     }
--- Server.java
+++ Server.java
@@ -3,37 +3,29 @@ import java.net.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
-        int port = 1234;
+        int port = 1234; // 2 mũ 16
 
-        /* Lấy địa chỉ IP của máy đang chạy server và in ra địa chỉ IP + port */
         String serverIP = InetAddress.getLocalHost().getHostAddress();
         System.out.println("Server chạy trên IP: " + serverIP + ", cổng: " + port);
 
-        /* Tạo một socket server */
         ServerSocket serverSocket = new ServerSocket(port);
 
-        System.out.println("Server chờ kết nối của client...");  
-        
         while (true) {
+            System.out.println("Server chờ kết nối của client...");  
             Socket clientSocket = serverSocket.accept();
             
-            /* In ra địa chỉ IP của client vừa kết nối */
-            String clientIP = clientSocket.getInetAddress().toString();  
+            String clientIP = clientSocket.getInetAddress().toString();
             System.out.println("Client kết nối: " + clientIP); 
 
-
-            /* Tạo luồng đọc và ghi để trao đổi dữ liệu với client */
             BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream(), "UTF-8"));
             BufferedWriter out = new BufferedWriter(new OutputStreamWriter(clientSocket.getOutputStream(), "UTF-8"));
 
-            /* Đọc dữ liệu client gửi tới và ép kiểu sang double */
             double a = Double.parseDouble(in.readLine());
             System.out.println("[" + clientIP + "] Gửi a = " + a);
             
             double b = Double.parseDouble(in.readLine());
             System.out.println("[" + clientIP + "] Gửi b = " + b);
 
-            /* Tính phương trình bậc nhất */
             String result;
             if (a == 0) {
                 result = (b == 0) ? "Vô số nghiệm" : "Vô nghiệm";
@@ -42,12 +34,10 @@ public class Server {
                 result = "Nghiệm x = " + x;
             }
 
-            /* In ra kết quả (.flush() đảm bảo dữ liệu được gửi đi ngay) */
             System.out.println("[" + serverIP + "] Gửi kết quả: " + result); 
             out.write(result + "\n"); 
             out.flush();
             
-            /* Đóng kết nối với client */
             clientSocket.close();
             System.out.println("[" + clientIP + "] Kết nối đã đóng.\n");
         }
